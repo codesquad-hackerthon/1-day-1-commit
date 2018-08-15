c25415b011bc96e7867407e9a0886cc29e7d1479
Ahreum Kim <kim.ah.rm@gmail.com>
2018-08-15 16:58:21 +0900

    create: login페이지 추

--- src/main/resources/templates/src/js/app.js
+++ src/main/resources/templates/src/js/app.js
@@ -6,8 +6,16 @@ import alarmDialog from './alarmDialog.js';
 
 const app = {
   userId: location.search.replace('?', '').split('userId=')[1],
+  loginForm: document.querySelector('#login-form'),
   init({ alarmDialog }) {
     alarmDialog.init({ userId: this.userId });
+
+    this.loginForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      const userId = this.loginForm.querySelector('[name="userId"]').value;
+      location.href = `/commits?userId=${userId}`;
+    });
   }
 };
 
